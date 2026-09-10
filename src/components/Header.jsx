4a5fd60b644812c39199f15943c637e275407653
BlessingJonathan <blessingjonathan028@gmail.com>
2026-09-10 15:30:00 +0200

    Updated images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
-import React  from "react";
+
 import styles from "../styles/Header.module.css";
 
 export default function Home() {
 
 
   const sections = [
-    { image: "public/image1.PNG" },
-    { image: "public/3.png" },
-    { image: "public/2.png" },
-    { image: "public/1.png" },
+    { image: "/image1.PNG" },
+    { image: "/3.png" },
+    { image: "/2.png" },
+    { image: "/1.png" },
     // { image: "/newimage.jpg" }, 
   ];
 
